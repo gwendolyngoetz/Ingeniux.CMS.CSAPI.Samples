dfbe671c877a27d8e255278884505b4107ff5302
Gwendolyn Goetz <gwendolyngoetz@users.noreply.github.com>
2017-09-28 14:01:46 -0700

    Update AssemblyInfo.cs

--- Ingeniux.CMS.CSAPI.Samples/Properties/AssemblyInfo.cs
+++ Ingeniux.CMS.CSAPI.Samples/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Ingeniux.CMS.CSAPI.Samples")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("City of Seattle")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Ingeniux.CMS.CSAPI.Samples")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
